64c61f0feb1480d94f83ef3e94005d5fddea7ed4
dabutvin <butvinik@outlook.com>
2019-06-29 10:56:07 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using ImageMagick;
+
+namespace test_compress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileInfo("zayn.png");
+            var imageOptimizer = new ImageOptimizer
+            {
+                OptimalCompression = true,
+                IgnoreUnsupportedFormats = true,
+            };
+
+            try
+            {
+                Console.WriteLine("Before " + file.Length);
+                imageOptimizer.LosslessCompress(file);
+                Console.WriteLine("This worked!!!");
+                Console.WriteLine("After " + file.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("it failed!");
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
